a34a15528af616957337e7eb5560722edf14417d
Henslaac <henslaac@gmail.com>
2024-12-04 13:14:05 +0200

    feat: add intent example validation
    The API has minimum requirements concerning the examples that are sent with the query string. There has to be at least two example intents and each intent has to have at least two examples.

--- 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/domain/intent_example_parser.py
+++ 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/domain/intent_example_parser.py
@@ -1,6 +1,19 @@
 import json
 from typing import List
+from collections import Counter
 from .schema import IntentExample
+
+
+
+def is_intent_example_list_valid(intent_example_list: List[IntentExample]) -> bool:
+    intent_counts = Counter(example.intent for example in intent_example_list)
+    if len(intent_counts) < 2:
+        print("Validation failed: Less than two types of intents exist.")
+        return False
+    if any(count < 2 for count in intent_counts.values()):
+        print("Validation failed: At least one intent has less than two examples.")
+        return False
+    return True
 
 
 class IntentExampleParser:
@@ -8,4 +21,8 @@ class IntentExampleParser:
     def from_json(file_path) -> List[IntentExample]:
         with open(file_path, 'r') as file:
             data = json.load(file)
-        return [IntentExample(**item) for item in data]
+        intent_example_list = [IntentExample(**item) for item in data]
+        if is_intent_example_list_valid(intent_example_list):
+            return intent_example_list
+        else:
+            raise ValueError("Intent examples do not satisfy minimum requirements")
