d8fd80919cded292f208f95c09ced4efd47e89d4
Henslaac <henslaac@gmail.com>
2024-12-04 14:14:37 +0200

    chore: demonstrate language switching

--- 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/main.py
+++ 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/main.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     # Example input text for which intents need to be detected
     example_input_text = [
         "The sun is out, the birds are singing. Let me go for a swim.",
-        "When I have time, I like to read."
+        "When I have time, I like to read.",
+        "Mina ngithanda ukudlala ibhola ngempelaviki.",
+        "Ngithanda ukufunda izingwalo eziphatha ulwazi.",
+        "Izolo ekuseni ngabona umphathi wethu ehofisini."
     ]
 
     # Create an intent detection request payload
@@ -34,5 +37,5 @@ if __name__ == "__main__":
     for i, intent_detection_result in enumerate(intent_detection_results):
         print(f"Intent detection for the input statement: '{example_input_text[i]}'")
         for probability in intent_detection_result.probabilities:
-            print(f"Intent: {probability.intent}\t\tScore: {probability.score}")
+            print(f"Intent: {probability.intent}\t\t\tScore: {probability.score}")
         print()
