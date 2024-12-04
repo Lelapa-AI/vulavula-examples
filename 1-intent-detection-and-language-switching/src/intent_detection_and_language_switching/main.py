34b9e1c7eccee8d85999a35c6b1e3c5bea6fe08a
Henslaac <henslaac@gmail.com>
2024-12-04 13:31:17 +0200

    chore: better print statements

--- 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/main.py
+++ 1-intent-detection-and-language-switching/src/intent_detection_and_language_switching/main.py
@@ -3,12 +3,15 @@ from client.vulavula_client import VulavulaClient
 from domain.intent_example_parser import IntentExampleParser
 from domain.schema.intent_detection import IntentDetectionRequest
 
+
+
 if __name__ == "__main__":
     intent_example_file_path = "data/intent_examples.json"
     intent_examples = IntentExampleParser.from_json(intent_example_file_path)
 
     example_input_text = [
-        "The sun is out, the birds are singing. Let me go for a swim."
+        "The sun is out, the birds are singing. Let me go for a swim.",
+        "When I have time, I like to read"
     ]
 
     intent_detection_request = IntentDetectionRequest(
@@ -20,4 +23,9 @@ if __name__ == "__main__":
     intent_detection_results = vulavula_client.send_intent_detection_request(intent_detection_request)
 
 
-    print(intent_detection_results)
+    print("Intent detection results...\n")
+    for i, intent_detection_result in enumerate(intent_detection_results):
+        print(f"Intent detections for the input statement: '{example_input_text[i]}'")
+        for probability in intent_detection_result.probabilities:
+            print(f"Intent: {probability.intent}\t\tScore: {probability.score}")
+        print()
